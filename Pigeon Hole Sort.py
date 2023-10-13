8e27dd5d58905c659e45ae82f8f618109f128f56
Bdub64 <90049502+Bdub64@users.noreply.github.com>
2023-10-13 12:07:01 -0400

    Added timers
    Added shuffle and sort timers first. For some reason, adding timer for printing increased time for sorting drastically (0.7ms => 1.9ms). Maybe just a quirk of python? More testing needed.

--- Pigeon Hole Sort.py
+++ Pigeon Hole Sort.py
@@ -1,28 +1,40 @@
 import random
 import numpy as np
+import time
+
 rngLst = input("Input the range you want shuffled and sorted (lower integer, higher integer, duplicates(True/False)) make sure to seperate the values with a comma and a space!\n").split(", ")
 
 rngMin = int(rngLst[0])
 rngMax = int(rngLst[1])
 
 rng = rngMax - rngMin + 1
+
+startShuffle = time.time()
+
 if rngLst[2] == 'True':
+
     lstToSort = np.random.choice([*range(rngMin, rngMax + 1)], rng, replace = True)
+    
 else: 
+
     lstToSort = [*range(rngMin, rngMax + 1)]
 
 random.shuffle(lstToSort)
 
+endShuffle = time.time()
+print("Shuffle took ", endShuffle - startShuffle, " seconds, or ", (endShuffle - startShuffle )* 1000, " miliseconds!")
 
 lst = [0] * rng
 
 print(lst)
 
-
+startSort = time.time()
 for i in lstToSort:
 
-    print(i)
     lst[i - 1] += 1
+
+endSort = time.time()
+print("Sorting took ", endSort - startSort, " seconds, or ", (endSort - startSort) * 1000, " miliseconds!")
 
 print(lst)
 
@@ -34,4 +46,10 @@ for i in range(len(lst)):
     tmpLst = [i + 1] * lst[i]
     lstToSort.extend(tmpLst)
 
+startPrint = time.time()
 print(lstToSort)
+endPrint = time.time()
+
+print("Shuffle took ", endShuffle - startShuffle, " seconds, or ", (endShuffle - startShuffle )* 1000, " miliseconds!")
+print("Sorting took ", endSort - startSort, " seconds, or ", (endSort - startSort) * 1000, " miliseconds!")
+print("printing took ", endPrint - startPrint, " seconds, or ", (endPrint - startPrint) * 1000, " miliseconds!")
