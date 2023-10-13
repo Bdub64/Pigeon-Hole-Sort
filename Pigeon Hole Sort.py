e944abf1d0a5a20908c1d8418540e6a61256744b
Bdub64 <90049502+Bdub64@users.noreply.github.com>
2023-10-13 11:48:18 -0400

    Done - will add timer for operations.
    This is Insanely fast; The part that takes the longest is actually the printing of that many characters.

--- Pigeon Hole Sort.py
+++ Pigeon Hole Sort.py
@@ -1,6 +1,37 @@
 import random
+import numpy as np
+rngLst = input("Input the range you want shuffled and sorted (lower integer, higher integer, duplicates(True/False)) make sure to seperate the values with a comma and a space!\n").split(", ")
 
-rngLst = input("Input the range you want shuffled and sorted (lower integer, higher integer) make sure to seperate the two values with a comma and a space!").split(", ")
+rngMin = int(rngLst[0])
+rngMax = int(rngLst[1])
 
-random.shuffle(range(rngLst[0], rngLst[1]))
+rng = rngMax - rngMin + 1
+if rngLst[2] == 'True':
+    lstToSort = np.random.choice([*range(rngMin, rngMax + 1)], rng, replace = True)
+else: 
+    lstToSort = [*range(rngMin, rngMax + 1)]
 
+random.shuffle(lstToSort)
+
+
+lst = [0] * rng
+
+print(lst)
+
+
+for i in lstToSort:
+
+    print(i)
+    lst[i - 1] += 1
+
+print(lst)
+
+
+lstToSort = []
+
+for i in range(len(lst)):
+
+    tmpLst = [i + 1] * lst[i]
+    lstToSort.extend(tmpLst)
+
+print(lstToSort)
